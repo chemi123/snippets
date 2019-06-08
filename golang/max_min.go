cafda205374d72ea8147e52231ec8c347a932879
Yuichi Tanimoto <ytanimot@yahoo-corp.jp>
2019-06-08 18:22:47 +0900

    max_min.go

---
+++ golang/max_min.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func max(nums ...int) int {
+	max := 0
+	for _, num := range nums {
+		max = int(math.Max(float64(max), float64(num)))
+	}
+	return max
+}
+
+func min(nums ...int) int {
+	min := math.MaxInt32
+	for _, num := range nums {
+		min = int(math.Min(float64(min), float64(num)))
+	}
+	return min
+}
+
+func main() {
+	fmt.Println(max(3, 5, 2, 4, 9, 7))
+	fmt.Println(min(3, 5, 2, 4, 9, 7))
+}
