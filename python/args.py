9d838a28a072a1e4c788e656fc8543299d21789c
Yuichi Tanimoto <>
2020-08-02 14:09:27 +0900

    fix arg.py

--- python/args.py
+++ python/args.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import random
 
-argvs = sys.argv
-argc = len(argvs)
-
-if argc != 2:
-    print('need an arg')
-    exit(1)
-
-print(argc)
-print(argvs)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 create_dataset.py ${dataset_num}")
+        exit(1)
+    
+    if not sys.argv[1].isdecimal():
+        print("input number for dataset_num")
+        exit(1)
