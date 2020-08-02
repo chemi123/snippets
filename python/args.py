dcfe77dda404e2879289184e78f2b27e7cb5c4e7
Yuichi Tanimoto <>
2020-08-02 14:11:28 +0900

    randint sample

--- python/args.py
+++ python/args.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import random
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
---
+++ python/randint.py
@@ -0,0 +1,5 @@
+import random
+
+if __name__ == "__main__":
+    # 1から100までをランダムに出力。シード値はデフォルトでunixtimeの現在時刻が使われているっぽい
+    print(random.randint(1, 100))
