3acfe152d6cf2358f799b5524a3a6940c0325722
Yuichi Tanimoto <>
2020-08-08 15:02:13 +0900

    fix union_find.cpp

--- cpp/algorithm/union_find/union_find.cpp
+++ cpp/algorithm/union_find/union_find.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,36 +8,68 @@ using namespace std;
   https://atcoder.jp/contests/atc001/tasks/unionfind_a
 */
 
-int find(int* root, int x) {
-  if (root[x] == x) {
-    return x;
+class UnionFind {
+public:
+  UnionFind(int n) : _parent(std::vector<int>(n, -1)) {
   }
-  return root[x] = find(root, root[x]);
-}
 
-bool same(int* root, int x, int y) {
-  return find(root, x) == find(root, y);
-}
+  int findRoot(int x) {
+    if (_parent[x] < 0) {
+      return x;
+    }
+    _parent[x] = findRoot(_parent[x]);
+    return _parent[x];
+  }
 
-void unite(int* root, int x, int y) {
-  x = find(root, x);
-  root[x] = find(root, y);
-}
+  void unite(int x, int y) {
+    x = findRoot(x);
+    y = findRoot(y);
+
+    if (x == y) {
+      return;
+    }
+
+    if (_parent[x] > _parent[y]) {
+      swap(x, y);
+    }
+
+    _parent[x] += _parent[y];
+    _parent[y] = x;
+  }
+
+  bool isSame(int x, int y) {
+    return findRoot(x) == findRoot(y);
+  }
+
+  bool getSize(int x) {
+    return -_parent[findRoot(x)];
+  }
+
+  int getGroupNum() {
+    int res = 0;
+    for (size_t i = 0; i < _parent.size(); ++i) {
+      if (_parent.at(i) < 0) {
+        ++res;
+      }
+    }
+    return res;
+  }
+
+private:
+  vector<int> _parent;
+};
 
 int main() {
   int n, q;
   cin >> n >> q;
-  int root[n+1];
-  for (int i = 0; i <= n; ++i) {
-    root[i] = i;
-  }
+  UnionFind uf(n);
   for (int i = 0; i < q; ++i) {
-    int judge, x, y;
-    cin >> judge >> x >> y;
-    if (judge) {
-      cout << (same(root, x, y) ? "Yes" : "No") << endl;
+    int query, x, y;
+    cin >> query >> x >> y;
+    if (query == 0) {
+      uf.unite(x, y);
     } else {
-      unite(root, x, y);
+      cout << (uf.isSame(x, y) ? "Yes" : "No") << endl;
     }
   }
   return 0;
