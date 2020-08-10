2c1624c3078d1d1b26ea6ffdc53369099d0b0a9b
Yuichi Tanimoto <>
2020-08-10 19:55:23 +0900

    use pair as key for set

---
+++ cpp/pair_key.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+struct phash{
+  inline size_t operator()(const pair<int,int> & p) const{
+    const auto h1 = hash<int>()(p.first);
+    const auto h2 = hash<int>()(p.second);
+    return h1 ^ (h2 << 1);
+  }
+};
+
+int main() {
+  unordered_set<pair<int, int>, phash> s;
+  s.emplace(make_pair(10, 20));
+  auto itr = s.find(make_pair(10, 20));
+  if (itr != s.end()) {
+    cout << itr->first << " " << itr->second << endl;
+  }
+  return 0;
+}
