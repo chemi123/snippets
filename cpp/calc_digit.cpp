ac532e33cfa1322cc6b18977904e64c04c93d08e
Yuichi Tanimoto <chemi@yuuichinoMacBook-Air.local>
2021-05-30 09:06:20 +0900

    calc_digit.cpp

---
+++ cpp/calc_digit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int num = 1;
+  cout << num << ": " << floor(log10(num)) + 1 << endl;
+  num = 10;
+  cout << num << ": " << floor(log10(num)) + 1 << endl;
+  num = 100;
+  cout << num << ": " << floor(log10(num)) + 1 << endl;
+  num = 1000;
+  cout << num << ": " << floor(log10(num)) + 1 << endl;
+  num = 20438;
+  cout << num << ": " << floor(log10(num)) + 1 << endl;
+  return 0;
+}
+
